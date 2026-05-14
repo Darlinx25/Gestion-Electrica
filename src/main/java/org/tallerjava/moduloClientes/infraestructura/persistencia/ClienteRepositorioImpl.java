be82a39990c37d86b0ddbb0a614828ad91977495
kevinjaffe <kevinjaffe@hotmail.com>
2026-05-14 22:47:59 +0000

    override listarClientes

--- src/main/java/org/tallerjava/moduloClientes/infraestructura/persistencia/ClienteRepositorioImpl.java
+++ src/main/java/org/tallerjava/moduloClientes/infraestructura/persistencia/ClienteRepositorioImpl.java
@@ -25,7 +25,8 @@ public class ClienteRepositorioImpl implements ClienteRepo {
     public Cliente buscaClientePorId(long id) {
         return em.find(Cliente.class,id);
     }
-
+    
+    @Override
     public List<Cliente> listarClientes(){
         return em.createQuery("SELECT c FROM Cliente c",Cliente.class).getResultList();
     }

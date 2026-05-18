93e5e628d74342c85eaf160512195bfa5d98f222
kevinjaffe <kevinjaffe@hotmail.com>
2026-05-18 03:52:05 +0000

    carga API post

--- src/main/java/org/tallerjava/moduloCargas/interfase/remota/CargaResourseAPI.java
+++ src/main/java/org/tallerjava/moduloCargas/interfase/remota/CargaResourseAPI.java
@@ -3,14 +3,16 @@ package org.tallerjava.moduloCargas.interfase.remota;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.tallerjava.moduloCargas.aplicacion.CargaServicios;
 import org.tallerjava.moduloCargas.dominio.EstacionCarga;
-import org.tallerjava.moduloClientes.dominio.Cliente;
+import org.tallerjava.moduloCargas.dominio.EstadoCargador;
+import org.tallerjava.moduloCargas.dominio.TipoCargador;
+import org.tallerjava.moduloCargas.dominio.TipoConector;
+import org.tallerjava.moduloCargas.interfase.CargadorDTO;
+import org.tallerjava.moduloCargas.interfase.EstacionDTO;
+
 
 import java.util.List;
 
@@ -32,4 +34,29 @@ public class CargaResourseAPI {
         return cargaService.obtenerEstaciones();
     }
 
-}
+
+    //registrar estacion
+    //curl -X POST -v http://localhost:8080/Gestion-Electrica/carga/cargas -H "Content-Type: application/json" -d '{"descripcion":"prueba estacion","calle":"Lenguas de Diamante","departamento":"Maldonado","longitud":2,"latitud":3}'
+
+    @POST
+    @Path("/estacion")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public long altaEstacion(EstacionDTO estacionDTO) {
+        return cargaService.altaEstacion(estacionDTO);
+    }
+
+
+    //registrar cargador
+    //curl -X POST -v http://localhost:8080/Gestion-Electrica/carga/cargador -H "Content-Type: application/json" -d '{"tipo": "RAPIDO", "tieneCable": true, "tipoConector": "CCS", "estado": "DISPONIBLE", "potenciaMinima": 150, "estacionId": 1}'
+    @POST
+    @Path("/cargador")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public long altaCargador(CargadorDTO cargadorDTO) {
+        return cargaService.altaCargador(cargadorDTO);
+    }
+    }
+
+
+
+
+

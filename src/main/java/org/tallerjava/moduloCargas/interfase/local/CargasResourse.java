cba3eb4d1ae12d7ec1370071ab0844565353f053
mizu43 <ignacio.ortega@estudiantes.utec.edu.uy>
2026-05-21 05:43:37 -0300

    interfaces locales

---
+++ src/main/java/org/tallerjava/moduloCargas/interfase/local/CargasResourse.java
@@ -0,0 +1,53 @@
+package org.tallerjava.moduloCargas.interfase.local;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.tallerjava.moduloCargas.aplicacion.CargaServicios;
+import org.tallerjava.moduloCargas.dominio.EstacionCarga;
+import org.tallerjava.moduloCargas.interfase.CargaDTO;
+import org.tallerjava.moduloCargas.interfase.CargadorDTO;
+import org.tallerjava.moduloCargas.interfase.EstacionDTO;
+import org.tallerjava.moduloCargas.interfase.EstadoCargaDTO;
+import org.tallerjava.moduloCargas.interfase.FinalizarCargaRequestDTO;
+import org.tallerjava.moduloCargas.interfase.IniciarCargaRequestDTO;
+
+@ApplicationScoped
+public class CargasResourse {
+
+    @Inject
+    private CargaServicios cargaServicios;
+    
+    public void iniciarCarga(IniciarCargaRequestDTO cargaDTO) {
+        cargaServicios.iniciarCarga(cargaDTO);
+    }
+    
+    public CargaDTO verCargaActual(long clienteId) {
+        return cargaServicios.verCargaActual(clienteId);
+    }
+    
+    public List<CargaDTO> verHistorico(Long clienteId, LocalDateTime ini, LocalDateTime fin) {
+        return cargaServicios.verHistorico(clienteId, ini, fin);
+    }
+    
+    public void finalizarCarga(FinalizarCargaRequestDTO cargaDTO) {
+        cargaServicios.finalizarCarga(cargaDTO);
+    }
+    
+    public void actualizarEstadoCarga(EstadoCargaDTO estadoCargaDTO) {
+        cargaServicios.actualizarEstadoCarga(estadoCargaDTO);
+    }
+    
+    public long altaEstacion(EstacionDTO estacionDTO) {
+        return cargaServicios.altaEstacion(estacionDTO);
+    }
+    
+    public long altaCargador(CargadorDTO cargadorDTO) {
+        return cargaServicios.altaCargador(cargadorDTO);
+    }
+    
+    public List<EstacionCarga> obtenerEstaciones() {
+        return cargaServicios.obtenerEstaciones();
+    }
+}
--- src/main/java/org/tallerjava/moduloPagos/interfase/local/PagosResourse.java
+++ src/main/java/org/tallerjava/moduloPagos/interfase/local/PagosResourse.java
@@ -1,10 +1,13 @@
 package org.tallerjava.moduloPagos.interfase.local;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import org.tallerjava.moduloPagos.dominio.Cliente;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.tallerjava.moduloCargas.interfase.CargaDTO;
 import org.tallerjava.moduloPagos.aplicacion.PagoServicios;
-import org.tallerjava.moduloPagos.dominio.MedioPago;
 
+@ApplicationScoped
 public class PagosResourse {
 
 
@@ -13,5 +16,9 @@ public class PagosResourse {
 
     public void pagarCarga(long clienteId, float importe, long medioPagoId){
         pagosServicios.pagarCarga(clienteId,importe,medioPagoId);
+    }
+    
+    public List<CargaDTO> consultarPagos(long clienteId, LocalDateTime ini, LocalDateTime fin) {
+        return pagosServicios.consultarPagos(clienteId, ini, fin);
     }
 }
